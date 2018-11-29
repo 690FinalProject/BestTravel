a7fb6e459271dfe034b0eec95b538fb67ac8c638
Jangey <jangey.lu@gmail.com>
2018-11-28 17:15:57 -0800

    Request API

--- BestTravel/ViewController.swift
+++ BestTravel/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let client_id = "FXPLHIF0E053NMZ0F2SLUPBKBHL15WHJQ2HGKBZPEVGXFFRR"
+        let client_secret = "J0A0KQAMFSGR4MM0GKXX04MQH312SSGFY2JVV35KQBHN3THH"
+
+        // get current date
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyymmdd"
+        let currentDate = formatter.string(from: date)
+        
+        
+        let urlString = "https://api.foursquare.com/v2/venues/explore?near=SF&client_id=\(client_id)&client_secret=\(client_secret)&v=\(currentDate)"
+        
+        Alamofire.request(urlString).responseJSON { response in
+            //debugPrint(response)
+            guard
+                let jsonDictionary = response.result.value as? [String: Any],
+                let response = jsonDictionary["response"] as? [String: Any],
+                let groups = response["groups"] as? [[String: Any]],
+                let items = groups[0]["items"] as? [[String: Any]],
+                
+                let venue = items[0]["venue"] as? [String: Any],
+                let name = venue["name"] as? String
+                else {
+                    return
+            }
+            print(name)
+        }
+        
+        print("test")
     }
 
 
