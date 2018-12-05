6e3729630e2f710705a14931e8b11285faa6bc48
Gordon <gordonsu9450100@gmail.com>
2018-12-05 03:58:08 -0800

    MapView

---
+++ BestTravel/MapScreen.swift
@@ -0,0 +1,33 @@
+//
+//  MapScreen.swift
+//  BestTravel
+//
+//  Created by Gordon on 12/5/18.
+//  Copyright © 2018 Jangey Lu. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapScreen: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
